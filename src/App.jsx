3e49f7ef67b226c2a1c8c5bf771fbe2084e9d10c
Vitaliy2773 <vitalii64773@gmail.com>
2023-10-22 19:33:52 -1100

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -16,41 +16,30 @@ export default class App extends Component {
     filter: '',
   };
 
-  handleChange = e => {
-    const { name, value } = e.target;
-    this.setState({
-      [name]: value,
-    });
-  };
-
   changeFilter = e => {
     this.setState({ filter: e.currentTarget.value });
   };
 
-  handleSubmit = e => {
-    const { name, number, contacts } = this.state;
+  handleSubmit = (contactName, contactNumber) => {
+    const { contacts } = this.state;
 
     const presentContact = contacts.find(
-      contact => contact.name.toLowerCase() === name.toLowerCase()
+      contact => contact.name.toLowerCase() === contactName.toLowerCase()
     );
 
     if (presentContact) {
-      alert(`${name} is already in contacts.`);
+      alert(`${contactName} is already in contacts.`);
       return;
     }
 
-    e.preventDefault();
-
     const newContact = {
       id: nanoid(),
-      name: name,
-      number: number,
+      name: contactName,
+      number: contactNumber,
     };
 
     this.setState(prevState => ({
       contacts: [...prevState.contacts, newContact],
-      name: '',
-      number: '',
     }));
   };
 
@@ -63,7 +52,7 @@ export default class App extends Component {
   inputId = nanoid();
 
   render() {
-    const { name, number, filter, contacts } = this.state;
+    const { filter, contacts } = this.state;
 
     const filteredContacts = contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
@@ -72,15 +61,8 @@ export default class App extends Component {
     return (
       <div className={css.container}>
         <h1 className={css.title}>Phonebook</h1>
-        <ContactForm
-          OnChangeName={this.handleChange}
-          onChangeNumber={this.handleChange}
-          onSubmit={this.handleSubmit}
-          name={name}
-          number={number}
-        />
+        <ContactForm onSubmit={this.handleSubmit} />
         <h2 className={css.title}>Contacts</h2>
-
         <Filter onChange={this.changeFilter} value={filter} />
         <ContactList
           contacts={filteredContacts}
--- src/components/Contacts/ContactForm.jsx
+++ src/components/Contacts/ContactForm.jsx
@@ -1,40 +1,54 @@
 import React from 'react';
 import css from './Contacts.module.css';
 
-export default function ContactForm({
-  OnChangeName,
-  onChangeNumber,
-  onSubmit,
-  number,
-  name,
-}) {
-  return (
-    <form onSubmit={onSubmit} className={css.form}>
-      <label>
-        Name
-        <input
-          type="text"
-          name="name"
-          onChange={OnChangeName}
-          value={name}
-          className={css.input}
-          required
-        />
-      </label>
-      <label>
-        Number
-        <input
-          type="tel"
-          name="number"
-          onChange={onChangeNumber}
-          value={number}
-          className={css.input}
-          required
-        />
-      </label>
-      <button type="submit" className={css.buttonAddContact}>
-        Add contact
-      </button>
-    </form>
-  );
+export default class ContactForm extends React.Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = e => {
+    const { name, value } = e.target;
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.name, this.state.number);
+    this.setState({ name: '', number: '' });
+  };
+
+  render() {
+    const { name, number } = this.state;
+
+    return (
+      <form onSubmit={this.handleSubmit} className={css.form}>
+        <label>
+          Name
+          <input
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={name}
+            className={css.input}
+            required
+          />
+        </label>
+        <label>
+          Number
+          <input
+            type="tel"
+            name="number"
+            onChange={this.handleChange}
+            value={number}
+            className={css.input}
+            required
+          />
+        </label>
+        <button type="submit" className={css.buttonAddContact}>
+          Add contact
+        </button>
+      </form>
+    );
+  }
 }
